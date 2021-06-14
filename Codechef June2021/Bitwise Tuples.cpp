c5f338841c95f9b346ba4dee54b39468f2552941
Shivani-Parihar99 <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-06-14 12:22:03 +0530

    Update Bitwise Tuples.cpp

--- Codechef June2021/Bitwise Tuples.cpp
+++ Codechef June2021/Bitwise Tuples.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+#define M 1000000007
+
+// Normal Method Complexity - O(n)
+// Complexity - O(logn)
+// Modular Exponention
+long long int power(long long int A, long long int B)
+{
+    if (B == 0)
+        return 1;
+
+    else if (B % 2 == 0)
+        return power((A*A) % M, B >> 1) % M;
+    else
+        return (A * power((A*A) % M, B >> 1) % M );
+}
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    long long int n, m, x, c, e;
+	    cin >> n >> m;
+	    c = power(2,n);
+	    x = (c - 1);
+	    e = power(x,m);
+	    cout << e <<endl;
+	}
+	return 0;
+}
+
 
