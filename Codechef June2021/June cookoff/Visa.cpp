3c27b0a0498cab2cb6e38c53cf956587799ce489
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-06-26 18:40:01 +0530

    Update Visa.cpp

--- Codechef June2021/June cookoff/Visa.cpp
+++ Codechef June2021/June cookoff/Visa.cpp
@@ -1,1 +1,7 @@
+/* Ash is trying to get visa to Chefland. For the visa to be approved, he needs to satisfy the following three criteria:
 
+Solve at least x1 problems on Codechef.
+Have at least y1 current rating on Codechef.
+Make his last submission at most z1 months ago.
+You are given the number of problems solved by Chef (x2), his current rating (y2) and the information that he made his last submission z2 months ago. 
+Determine whether he will get the visa.*/
