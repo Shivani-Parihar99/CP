3d175038081a5e47ad5f69eb6dd48e8f28f86a4c
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-07-04 12:55:00 +0530

    Create MINNOTES.cpp

---
+++ Codechef July 2021/MINNOTES.cpp
@@ -0,0 +1,5 @@
+/* You are the owner of a big company. You are so rich, that the government has allowed you to print as many notes as you want of any single value that you like.
+You also have peculiar behavioral traits and you often do things that look weird to a third person.You have N employees, where the i-th employee has salary Ai.
+You want to pay them using a denomination that you create. You are also eco-friendly and wish to save paper. So, you wish to pay them using as few notes as possible.
+Find out the minimum number of notes required if you can alter the salary of at most one employee to any positive integer that you like, and choose the positive integer
+value that each note is worth (called its denomination).Each employee must receive the exact value of his/her salary and no more. */
