75957fdbc1b0d0db7a286667783722c07ad585d6
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-07-04 15:15:21 +0530

    Update maxproduction.cpp

--- Codechef July 2021/maxproduction.cpp
+++ Codechef July 2021/maxproduction.cpp
@@ -2,3 +2,19 @@
 There are two ways he can spend his energy during the week. The first way is to do x units of work every day and the second way is to do y (>x) units of work for the
 first d (<7) days and to do z (<x) units of work thereafter since he will get tired of working more in the initial few days.
 Find the maximum amount of work he can do during the week if he is free to choose either of the two strategies.*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int d,x,y,z;
+	    cin>>d>>x>>y>>z;
+	    cout << max((x*7),((y*d)+(z*(7-d))))<<endl;
+	}
+	return 0;
+}
+
