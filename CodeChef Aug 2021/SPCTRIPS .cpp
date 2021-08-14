1e79c457f85c2b8a8643596acf3b36cd4071fa27
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-08-14 18:35:52 +0530

    Update SPCTRIPS .cpp

--- CodeChef Aug 2021/SPCTRIPS .cpp
+++ CodeChef Aug 2021/SPCTRIPS .cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    int cnt = 0;
+	    for(int c=1; c<=n; c++){
+	        for(int b=2*c; b<=n; b+=c){
+	            cnt += ((n-c)/b) + 1;
+	        }
+	    }
+	    
+	    cout << cnt << endl;
+	}
+	return 0;
+}
