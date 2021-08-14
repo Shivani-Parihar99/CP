59bf4b4feb818010c8cbd2a4b1839400d1eec70d
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-08-14 18:34:11 +0530

    Update CHFINVNT.cpp

--- CodeChef Aug 2021/CHFINVNT.cpp
+++ CodeChef Aug 2021/CHFINVNT.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	// your code goes here
+	ll t;
+	cin >> t;
+	while(t--){
+	    ll n, p, k;
+	    cin >> n >> p >> k;
+	    ll ans;
+	    ll q = p%k;
+	    if(n%k <= q-1){
+	        ans = (n%k)*(n/k+1)+(q-n%k)*(n/k) + (p/k+1);
+	    }
+	    else{
+	        ans = q*(n/k+1)+(p/k+1);
+	    }
+	    cout << ans << endl;
+	}
+	return 0;
+}
 
